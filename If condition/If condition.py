38fe8b67da54256a005ad1eb7eac25da81cf5d34
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-17 10:20:30 +0530

    Update If condition.py

--- If condition/If condition.py
+++ If condition/If condition.py
@@ -1,1 +1,16 @@
-
+# Direct question....:)
+x = 5
+if x==5:
+  x=10
+  
+elif x==10:
+  x=15
+ 
+elif x==15:
+  x=20
+  
+else:
+  x=100
+  
+# now what is the answer of this program....
+# when x=10 what is the answer....
