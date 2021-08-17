d9ce03ad808cfe35e073cf8569f22b6823b69bfb
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-17 10:29:14 +0530

    Update odd | even | another number.py

--- odd | even | another decimal numer using loop and if condition/odd | even | another number.py
+++ odd | even | another decimal numer using loop and if condition/odd | even | another number.py
@@ -1,1 +1,19 @@
+l=[1,2,3,50,12,3,4,56,6,84,54,35,65]
+l2=[]
+l3=[]
+l4=[]
 
+for i in l :
+  
+  if i%2 == 1:
+    l2.append(i)
+    return l2
+  
+  elif i%2 == 0:
+    l3.append(i)
+    return l3
+  
+  else:
+    l4.append(i)
+    return l4
+   
