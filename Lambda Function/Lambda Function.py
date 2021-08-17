3759d82122e050bf24dc3362d4e5c2b9867a8e3e
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-17 12:13:03 +0530

    Update Lambda Function.py

--- Lambda Function/Lambda Function.py
+++ Lambda Function/Lambda Function.py
@@ -1,1 +1,16 @@
+# power of number using lambda function
+power = lambda   x , y : x**y
+# ^       ^        ^      ^
+# |       |        |      |
+# name   name     How    what was the output we are looking for.. i
+#         of      many    |
+#       function   |      v
+#               inputs  Logic
 
+# without lambda function
+def power (x,y):
+  return x**y
+# give input
+power(2,5)
+
+  
