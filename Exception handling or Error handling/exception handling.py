eadc1a9c4f1ad1549b1848e6c8cedf53a0ef669d
heet bhalala <44464602+heet7@users.noreply.github.com>
2021-08-19 03:00:47 +0530

    Update exception handling.py

--- Exception handling or Error handling/exception handling.py
+++ Exception handling or Error handling/exception handling.py
@@ -1,1 +1,23 @@
+# exception handling
+def fun(x,y):
+    
+    try:
+        c = x / y
+        return c
+    
+    except:
+        print("we got an error")
+        
+    finally:
+        print("we will excute this part anyway")
+        
+        
+fun(10,2)
+# answer
+# we will excute this part anyway
+# 5.0
 
+# fun(10,0)
+# answer
+# we got an error
+# we will excute this part anyway
